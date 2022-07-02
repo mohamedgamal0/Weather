4a5003ea8d6ed2530cd50b5b837ecf9346268699
Mohamed Gamal <mohamed.gamal@areebgroup.com>
2022-07-02 16:15:17 +0200

    added Parallax Effect

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -9,77 +9,101 @@ import SwiftUI
 import BottomSheet
 
 enum BottomSheetPosition: CGFloat, CaseIterable {
-    case top = 0.83 // 702 / 844
-    case middle = 0.385 // 325 / 844
+    case top = 0.83 // 702/844
+    case middle = 0.385 // 325/844
 }
 
 struct HomeView: View {
     @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDragged: Bool = false
+    
+    var bottomSheetTranslationProrated: CGFloat {
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
     
     var body: some View {
         NavigationView {
-            ZStack {
-                // MARK: - Background Color
-                Color.background
-                    .ignoresSafeArea()
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
                 
-                // MARK: - Background Image
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                // MARK: - House Image
-                Image("House")
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .padding(.top, 257)
-                
-                // MARK: - Current Weather
-                VStack(spacing: -10) {
-                    Text("Montreal")
-                        .font(.largeTitle)
+                ZStack {
+                    // MARK: Background Color
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack {
+                    // MARK: Background Image
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    // MARK: House Image
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    // MARK: Current Weather
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProrated)) {
+                        Text("Montreal")
+                            .font(.largeTitle)
                         
-                        Text(attributedString)
+                        VStack {
+                            Text(attributedString)
+                            
+                            Text("H:24°   L:18°")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                        }
                         
-                        Text("H:24°   L:18°")
-                            .font(.title3.weight(.semibold))
+                        Spacer()
+                    }
+                    .padding(.top, 51)
+                    .offset(y: -bottomSheetTranslationProrated * 46)
+                    
+                    // MARK: Bottom Sheet
+                    BottomSheetView(position: $bottomSheetPosition) {
+//                        Text(bottomSheetTranslationProrated.formatted())
+                    } content: {
+                        ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProrated)
+                    }
+                    .onBottomSheetDrag { translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        
+                        withAnimation(.easeInOut) {
+                            if bottomSheetPosition == BottomSheetPosition.top {
+                                hasDragged = true
+                            } else {
+                                hasDragged = false
+                            }
+                        }
                     }
                     
-                    Spacer()
+                    // MARK: Tab Bar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
                 }
-                .padding(.top, 51)
-                
-                // MARK: - Bottom Sheet
-                BottomSheetView(position: $bottomSheetPosition) {
-//                    Text(bottomSheetPosition.rawValue.formatted())
-                } content: {
-                    ForecastView()
-                }
-
-
-                // MARK: - Tab Bar
-                TabBar(action: {
-                    bottomSheetPosition = .top
-                })
             }
-            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
         }
     }
     
     private var attributedString: AttributedString {
-        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        var string = AttributedString("19°" + (hasDragged ? " | " : "\n ") + "Mostly Clear")
         
         if let temp = string.range(of: "19°") {
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProrated * (96 - 20))), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
         }
         
         if let pipe = string.range(of: " | ") {
             
             string[pipe].font = .title3.weight(.semibold)
-            string[pipe].foregroundColor = .secondary
-
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranslationProrated)
         }
         
         if let weather = string.range(of: "Mostly Clear") {
